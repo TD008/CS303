71e72ca9de3abb3e960fe350f2f23162168d29ab
Trent Davis <trentd@uab.edu>
2023-06-13 23:04:04 -0500

    Initial setup of lab01 assignment

--- lab01/Davis_Trenton_Lab01.java
+++ lab01/Davis_Trenton_Lab01.java
@@ -2,10 +2,22 @@ import java.io.*;
 
 public class Davis_Trenton_Lab01{
 	
+    public static void main(String[] args[]){
 
-	public static void main(String[] args[]){
-	
+
+    	
 	}	
+
+
+    public static void algorithmImplement() {
+    /* Class to hold the method that implements the insertion sort algorithm */
+
+        public static int[] implementInsertSort(int[] arr){
+            
+            
+        }
+
+    }
 }
 
 
